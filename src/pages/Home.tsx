2613e56392fffd1ea7448092b643e94a98222915
Bugra <bgryks18@gmail.com>
2023-11-14 23:29:02 +0300

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        ...
+      </button>
+    </div>
+  )
+}
+
+export default Home
